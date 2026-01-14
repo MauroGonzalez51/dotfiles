41c2f7e1a02d2aeaa3239aaa1e975f3608d23a54
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2026-01-13 21:31:13 -0500

    chore: removing yazi config due to conflicts

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,3 +3,33 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- Configuración de formateo
+lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json", "*.css", "*.html" }
+
+-- Configurar null-ls para ESLint y Prettier
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup({
+  {
+    command = "prettier",
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "css", "scss", "html" },
+  },
+})
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup({
+  {
+    command = "eslint",
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  },
+})
+
+-- Configurar code actions de ESLint
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup({
+  {
+    command = "eslint",
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  },
+})
