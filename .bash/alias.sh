c63de91d080f61e6012f3623b9c5f01fb85d8138
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2026-01-24 19:07:59 -0500

    feat: adding some aliases

--- .bash/alias.sh
+++ .bash/alias.sh
@@ -1,3 +1,4 @@
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
+alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
