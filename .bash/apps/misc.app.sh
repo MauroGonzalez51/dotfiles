a971eb85ccf0d18c21a65e945ae83e33a76eaa08
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2026-01-30 11:37:41 -0500

    chore: packing misc apps/cli config into apps/misc

--- .bash/apps/misc.app.sh
+++ .bash/apps/misc.app.sh
@@ -1,3 +1,7 @@
+if command -v shopt >/dev/null 2>&1; then
+    shopt -s autocd
+fi
+
 if [ -f  /usr/share/doc/pkgfile/command-not-found.bash ]; then
     source /usr/share/doc/pkgfile/command-not-found.bash
 fi
