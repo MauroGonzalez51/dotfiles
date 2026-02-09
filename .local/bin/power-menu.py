1cb05e58862023a682ded6efe86756a36cb467bd
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2026-02-09 12:13:23 -0500

    chore: more styling

--- .local/bin/power-menu.py
+++ .local/bin/power-menu.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
 
 import subprocess
-from enum import Enum
 from dataclasses import dataclass
+from enum import Enum
+from typing import List
 
 
 @dataclass
 class PowerOption:
     label: str
     icon: str
-    command: str
+    command: List[str]
 
 
 class PowerActions(Enum):
     SHUTDOWN = PowerOption(
-        label="Shutdown", icon="\uf011", command="systemctl poweroff"
+        label="Shutdown", icon="\uf011", command=["systemctl", "poweroff"]
     )
 
     REBOOT = PowerOption(
-        label="Reboot", icon="\udb81\udc53", command="systemctl reboot"
+        label="Reboot", icon="\udb81\udc53", command=["systemctl", "reboot"]
     )
 
     LOGOUT = PowerOption(
-        label="Log Out", icon="\udb81\uddfd", command="hyprctl dispatch exit"
+        label="Log Out", icon="\udb81\uddfd", command=["hyprctl", "dispatch", "exit"]
     )
 
-    SUSPEND = PowerOption(label="Suspend", icon="\uf186", command="systemctl suspend")
+    SUSPEND = PowerOption(
+        label="Suspend", icon="\uf186", command=["systemctl", "suspend"]
+    )
 
-    LOCK = PowerOption(label="Lock", icon="\uea75", command="hyprlock")
+    LOCK = PowerOption(label="Lock", icon="\uea75", command=["hyprlock"])
 
 
 def main() -> None:
@@ -53,7 +56,7 @@ def main() -> None:
                 option = action.value
 
                 if f"{option.icon} {option.label}" == choice:
-                    subprocess.run(option.command, shell=True)
+                    subprocess.run(option.command)
                     break
     except Exception as exception:
         print(f"[exception] {exception}")
