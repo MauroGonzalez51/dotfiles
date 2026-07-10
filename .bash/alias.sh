5e2970f0a5f7316d1502abce0fd8a84cf7711b44
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2026-07-10 18:11:05 -0500

    chore: smol alias to sudo so it notifies when it requires the password

--- .bash/alias.sh
+++ .bash/alias.sh
@@ -2,3 +2,11 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
+
+sudo() {
+    if ! command sudo -n true 2>/dev/null; then
+        notify-send -u critical "Password Required"
+    fi
+
+    command sudo "$@"
+}
