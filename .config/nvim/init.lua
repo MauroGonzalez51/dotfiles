03abcdc36fcbfa2ab83b9a6ac96083f80297cd92
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2026-07-19 09:26:10 -0500

    chore: removed config in favor of lazy.vim D:

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,2 @@
--- disable native nvim file explorer
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- enable 24-bit RGB color in the TUI
-vim.opt.termguicolors = true
-
--- clipboard integration
-vim.opt.clipboard:append('unnamedplus')
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-
-require('config.lazy')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
